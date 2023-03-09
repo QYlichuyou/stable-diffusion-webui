3b7467a1d9d1a3649545f2c1db89a1720f19af44
QYlichuyou <114558948+QYlichuyou@users.noreply.github.com>
2023-03-09 20:43:09 +0800

    Update webui.sh

--- webui.sh
+++ webui.sh
@@ -118,7 +118,7 @@ case "$gpu_info" in
 esac
 if echo "$gpu_info" | grep -q "AMD" && [[ -z "${TORCH_COMMAND}" ]]
 then
-    export TORCH_COMMAND="pip install torch torchvision --extra-index-url https://download.pytorch.org/whl/rocm5.2"
+    export TORCH_COMMAND="pip install torch torchvision --extra-index-url https://mirrors.aliyun.com/pypi/simple/"
 fi  
 
 for preq in "${GIT}" "${python_cmd}"
@@ -148,7 +148,7 @@ else
     printf "\n%s\n" "${delimiter}"
     printf "Clone stable-diffusion-webui"
     printf "\n%s\n" "${delimiter}"
-    "${GIT}" clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git "${clone_dir}"
+    "${GIT}" clone https://ghproxy.com/https://github.com/AUTOMATIC1111/stable-diffusion-webui.git "${clone_dir}"
     cd "${clone_dir}"/ || { printf "\e[1m\e[31mERROR: Can't cd to %s/%s/, aborting...\e[0m" "${install_dir}" "${clone_dir}"; exit 1; }
 fi
 
